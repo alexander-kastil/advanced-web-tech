b220b555bb7229bb2cb297a40711bd88bf5e0e4e
Alexander Pajer <alexander.pajer@integrations.at>
2023-07-26 09:57:57 +0200

    sql

--- demos/08-container/03-docker/02-build-img/config-api/Program.cs
+++ demos/08-container/03-docker/02-build-img/config-api/Program.cs
@@ -1,17 +1,21 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using SkillsApi;
 
 // Configure Services
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-var configuration = new ConfigurationBuilder()
+IConfiguration configuration = new ConfigurationBuilder()
   .AddJsonFile("appsettings.json")
   .AddEnvironmentVariables()
   .Build();
+builder.Services.AddSingleton<IConfiguration>(configuration);
+var cfg = configuration.Get<AppConfig>();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -23,6 +27,15 @@ builder.Services.AddCors(o => o.AddPolicy("nocors", builder =>
         .AllowAnyHeader()
         .AllowCredentials();
 }));
+
+if (cfg.App.UseSQLite)
+{
+    builder.Services.AddDbContext<SkillDBContext>(options => options.UseSqlite(cfg.App.ConnectionStrings.SQLiteDBConnection));
+}
+else
+{
+    builder.Services.AddDbContext<SkillDBContext>(opts => opts.UseSqlServer(cfg.App.ConnectionStrings.SQLServerConnection));
+}
 
 // Open Api
 builder.Services.AddEndpointsApiExplorer();
