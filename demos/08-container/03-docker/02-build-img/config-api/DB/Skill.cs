5c853ff026a0a3bdf8e1e398795bd1942a61bd05
Alexander Pajer <alexander.pajer@integrations.at>
2023-07-26 10:05:50 +0200

    data seeding

--- demos/08-container/03-docker/02-build-img/config-api/DB/Skill.cs
+++ demos/08-container/03-docker/02-build-img/config-api/DB/Skill.cs
@@ -6,12 +6,10 @@ namespace SkillsApi
     public class Skill
     {
         public int Id { get; set; }
-
         [RequiredAttribute]
         public string Title { get; set; }
         public int Hours { get; set; }
         public bool Completed { get; set; }
         public DateTime DueDate { get; set; }        
-
     }
 }
--- demos/08-container/03-docker/02-build-img/config-api/DB/SkillDBContext.cs
+++ demos/08-container/03-docker/02-build-img/config-api/DB/SkillDBContext.cs
@@ -4,23 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace SkillsApi {
+namespace SkillsApi
+{
     //To manage Migrations & create the DB go to console:
     //[dotnet restore]
     //dotnet ef migrations add MIGRATION-NAME
     //dotnet ef database update
 
-    public class SkillDBContext : DbContext {
-        public SkillDBContext () {
+    public class SkillDBContext : DbContext
+    {
+        public SkillDBContext()
+        {
 
         }
 
-        public SkillDBContext (DbContextOptions<SkillDBContext> options) : base (options) {
-
+        public SkillDBContext(DbContextOptions<SkillDBContext> options) : base(options)
+        {
+            Database.EnsureCreated();
         }
-        
+
         public DbSet<Skill> Skills { get; set; }
 
-        protected override void OnModelCreating (ModelBuilder modelBuilder) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var sk1 = new Skill {Id=1, Title = "Custom Theme", Completed = true, Hours = 4, DueDate = DateTime.Now.AddMonths(-1) };
+            var sk2 = new Skill {Id=2, Title = "Theme Mixins", Completed = false, Hours = 3, DueDate = DateTime.Now.AddMonths(-2) };
+            var sk3 = new Skill {Id=3, Title = "Light & Dark Theme", Completed = false, Hours = 2, DueDate = DateTime.Now.AddMonths(2) };
+            var sk4 = new Skill {Id=4, Title = "RxJS Operators", Completed = true, Hours = 5, DueDate = DateTime.Now.AddDays(2) };
+            var sk5 = new Skill {Id=5, Title = "Custom Operators", Completed = false, Hours = 1, DueDate = DateTime.Now.AddYears(1) };
+            modelBuilder.Entity<Skill>().HasData(sk1, sk2, sk3, sk4, sk5);
+        }
     }
 }
